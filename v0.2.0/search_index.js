13f444ff3ef630ba69adc5c617d86519516a3efb
Documenter.jl <documenter@juliadocs.github.io>
2024-07-22 14:51:15 +0000

    build based on 83323de

---
+++ v0.2.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SharedPreferences","page":"Home","title":"SharedPreferences","text":"","category":"section"},{"location":"#Installation","page":"Home","title":"Installation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"using Pkg\nPkg.add(\"SharedPreferences\")","category":"page"}]
+}
