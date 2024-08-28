a0f32ceb9b890415b9c4ef6b03b26e1879f14e83
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-08-28 14:02:53 -0300

    Add precompile tools

--- src/SharedPreferences.jl
+++ src/SharedPreferences.jl
@@ -1,10 +1,31 @@
 module SharedPreferences
 
 using Nettle
+using PrecompileTools: @compile_workload
 using TOML
 
 export set!, get, remove!, clean!
 
 include("instance.jl")
+
+@setup_workload begin
+    @compile_workload begin
+        key = "Q0K38aFg7aSbVvDAgnDhgBF1CWyTfLlH"
+    end
+
+    instance = SharedPreferences.InstanceEncrypted(key)
+
+    set!(instance, "string", "value")
+    set!(instance, "integer", 42)
+    set!(instance, "float", 3.14)
+    set!(instance, "boolean", true)
+    set!(instance, "array", [1, 2, 3])
+
+    stored_string = get(instance, "string")
+    stored_integer = get(instance, "integer")
+    stored_float = get(instance, "float")
+    stored_boolean = get(instance, "boolean")
+    stored_array = get(instance, "array")
+end
 
 end
